456a3291cf37e0b9fa97563323b669c3a4e6911f
Leonardo Silvio de Moura <leonardosimoura@gmail.com>
2022-04-10 21:43:43 -0300

    Update to dotnet 6

--- BackEndPessoa/Program.cs
+++ BackEndPessoa/Program.cs
@@ -1,54 +1,102 @@
+using BackEndPessoa.Database;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 
-namespace BackEndPessoa
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+var builder = WebApplication.CreateBuilder(args);
+
+const string OtlpExporterEndpoint = "http://localhost:8200";
+
+builder.WebHost.ConfigureKestrel(options =>
 {
-    public class Program
+    options.Listen(IPAddress.Any, 5051, listenOptions =>
     {
-        public static void Main(string[] args)
-        {
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            CreateHostBuilder(args).Build().Run();
-        }
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddOpenTelemetry(options =>
-                    {
-                        options.IncludeFormattedMessage = true;
-                        options.IncludeScopes = true;
-                        options.ParseStateValues = true;
-                        options.AddOtlpExporter(configure =>
-                        {
-                            configure.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
-                            configure.Endpoint = new Uri("http://localhost:8200");
-                            //configure.Headers = "Authorization=Bearer {apm_secret_token}";
-                        });
-                    });
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel(options =>
-                    {
-                        options.Listen(IPAddress.Any, 5051, listenOptions =>
-                        {
-                            listenOptions.Protocols = HttpProtocols.Http1;
-                        });
-                    });
 
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeFormattedMessage = true;
+    options.IncludeScopes = true;
+    options.ParseStateValues = true;
+    options.AddOtlpExporter(configure =>
+    {
+        configure.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        configure.Endpoint = new Uri(OtlpExporterEndpoint);
+        //configure.Headers = "Authorization=Bearer {apm_secret_token}";
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BackEndPessoa", Version = "v1" });
+});
+
+builder.Services.AddDbContext<BackEndPessoaContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("BackEndPessoaContext")));
+
+
+builder.Services.AddOpenTelemetryTracing((builder) => builder
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("BackEndPessoa"))
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .AddEntityFrameworkCoreInstrumentation((config) =>
+    {
+        config.SetDbStatementForStoredProcedure = true;
+        config.SetDbStatementForText = true;
+    })
+    .AddZipkinExporter(o =>
+    {
+        o.Endpoint = new Uri(@"http://localhost:9411/api/v2/spans");
+    })
+    .AddJaegerExporter(config =>
+    {
+        config.AgentHost = "localhost";
+        config.AgentPort = 6831;
+    })
+     .AddOtlpExporter(configure =>
+     {
+         configure.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+         configure.Endpoint = new Uri("http://localhost:8200");
+         //configure.Headers = "Authorization=Bearer {apm_secret_token}";
+     }));
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BackEndPessoa v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,53 +1,99 @@
+using FrontEnd.Tracing;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
+using System.Text.Json;
 
-namespace FrontEnd
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+var builder = WebApplication.CreateBuilder(args);
+
+const string OtlpExporterEndpoint = "http://localhost:8200";
+
+builder.WebHost.ConfigureKestrel(options =>
 {
-    public class Program
+    options.Listen(IPAddress.Any, 5053, listenOptions =>
     {
-        public static void Main(string[] args)
-        {
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            CreateHostBuilder(args).Build().Run();
-        }
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddOpenTelemetry(options =>
-                    {
-                        options.IncludeFormattedMessage = true;
-                        options.IncludeScopes = true;
-                        options.ParseStateValues = true;
-                        options.AddOtlpExporter(configure =>
-                        {
-                            configure.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
-                            configure.Endpoint = new Uri("http://localhost:8200");
-                            //configure.Headers = "Authorization=Bearer {apm_secret_token}";
-                        });
-                    });
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel(options =>
-                    {
-                        options.Listen(IPAddress.Any, 5053, listenOptions =>
-                        {
-                            listenOptions.Protocols = HttpProtocols.Http1;
-                        });
-                    });
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeFormattedMessage = true;
+    options.IncludeScopes = true;
+    options.ParseStateValues = true;
+    options.AddOtlpExporter(configure =>
+    {
+        configure.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        configure.Endpoint = new Uri(OtlpExporterEndpoint);
+        //configure.Headers = "Authorization=Bearer {apm_secret_token}";
+    });
+});
+
+
+builder.Services.AddRazorPages()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    });
+
+builder.Services.AddOpenTelemetryTracing((builder) => builder
+   .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                        .AddService("FrontEnd", serviceVersion: "1.0")
+                        )
+   .AddSource(TracingHelper.KEY)
+   .AddAspNetCoreInstrumentation()
+   .AddHttpClientInstrumentation()
+   .
+   .AddZipkinExporter(o =>
+   {
+       o.Endpoint = new Uri(@"http://localhost:9411/api/v2/spans");
+   })
+   .AddJaegerExporter(config =>
+   {
+       config.AgentHost = "localhost";
+       config.AgentPort = 6831;
+   })
+   .AddOtlpExporter(configure =>
+   {
+       configure.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+       configure.Endpoint = new Uri(OtlpExporterEndpoint);
+       //configure.Headers = "Authorization=Bearer {apm_secret_token}";
+   }));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+});
+
+app.Run();
